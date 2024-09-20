877e175dbe5c9d2df7307d19b38121c749019e45
mattg07 <matiasmgodoy00@gmail.com>
2024-09-20 15:36:21 +0200

    cleanup and added gitignore

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const uniqid = require('uniqid');
+const fs = require('fs');
+
+
 
 const app = express();
 
 app.get('/test', (req,res) => {
     return res.json('test ok');
 })
+
+app.get('/create-story', (req,res) => {
+const url = req.query.url;
+const dir = uniqid();
+const path = './stories/'+dir;
+fs.mkdirSync(path, {recursive:true})
+
+
+console.log({
+    url,
+}) 
+return res.json('ok')
+})
+
+
 
 app.listen(8080, () => console.log('Listening on port 8080'))
